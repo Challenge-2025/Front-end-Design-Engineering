4593c48813332dd49e71bc93059d387ced06f3b2
Ferreira <pedroferreira.gomes03@gmail.com>
2025-05-07 22:08:50 -0300

    finalizando organizacao de cores do site e adicionando javascript

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
+const btnAbrirMenu = document.querySelector(".btn-menu-sanduiche");
+const menu = document.querySelector(".menu");
+const btnFecharMenu = document.querySelector(".btn-fechar-menu");
 
+btnAbrirMenu.addEventListener("click", () => {
+  menu.style.display = "flex";
+  void menu.offsetWidth;
+  menu.classList.add("mostrando");
+});
+
+btnFecharMenu.addEventListener("click", () => {
+  menu.classList.remove("mostrando");
+  setTimeout(() => {
+    menu.style.display = "none";
+  }, 400);
+});
