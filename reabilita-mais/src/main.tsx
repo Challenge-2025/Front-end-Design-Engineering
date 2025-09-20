3e47b8b4171a5f213de6c92ee797b19c40194699
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-09-20 20:21:31 -0300

    Remove dupla chamada do DOM.

--- reabilita-mais/src/main.tsx
+++ reabilita-mais/src/main.tsx
@@ -1,18 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App.tsx";
 import Home from "./routes/Home/index.tsx";
 import Error from "./routes/Error/index.tsx";
 import FaleConosco from "./routes/FaleConosco/index.tsx";
-import "./globals.css";
 import Participantes from "./routes/Participantes/index.tsx";
 import Login from "./routes/Login/index.tsx";
 import Cadastro from "./routes/Cadastro/index.tsx";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
+import "./globals.css";
 
 const router = createBrowserRouter([
   {
@@ -29,16 +27,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
 );
