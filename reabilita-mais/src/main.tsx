360af94230b66cce96b489e710134e94a878d14b
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-09-29 20:53:33 -0300

    Remove rota "Fale conosco".

--- reabilita-mais/src/main.tsx
+++ reabilita-mais/src/main.tsx
@@ -6,7 +6,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import Home from "./routes/Home/index.tsx";
 import Error from "./routes/Error/index.tsx";
-import FaleConosco from "./routes/FaleConosco/index.tsx";
 import Participantes from "./routes/Participantes/index.tsx";
 import Login from "./routes/Login/index.tsx";
 import Cadastro from "./routes/Cadastro/index.tsx";
@@ -20,7 +19,6 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/faleConosco", element: <FaleConosco /> },
       { path: "/participantes", element: <Participantes /> },
       { path: "/login", element: <Login /> },
       { path: "/cadastro", element: <Cadastro /> },
