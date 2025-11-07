c7cdcc564d48a301529d056040adc851c5f13453
Ferreira <pedroferreira.gomes03@gmail.com>
2025-11-07 13:36:53 -0300

    feat: adicionando seguranca na MAIN

--- reabilita-mais/src/routes/Layouts/PrivateLayout/RotasPrivadas/index.tsx
+++ reabilita-mais/src/routes/Layouts/PrivateLayout/RotasPrivadas/index.tsx
@@ -1,14 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../Hook/useAuth";
+import { useAuth } from "../../Hook/useAuth";
 
 export default function RotaPrivada() {
   const { paciente } = useAuth();
 
-  // Se não estiver logado, redireciona para /login
   if (!paciente) {
     return <Navigate to="/login" replace />;
   }
 
-  // Se estiver logado, renderiza normalmente
   return <Outlet />;
 }
