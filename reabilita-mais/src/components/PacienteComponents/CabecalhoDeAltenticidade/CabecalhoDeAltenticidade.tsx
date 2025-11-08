f56a3f9eb8dd3ec7844f703e24281d74d6571cd3
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-11-07 22:12:12 -0300

    feat: Adiciona integração da API Java ao cadastro do cliente.

--- reabilita-mais/src/components/PacienteComponents/CabecalhoDeAltenticidade/CabecalhoDeAltenticidade.tsx
+++ reabilita-mais/src/components/PacienteComponents/CabecalhoDeAltenticidade/CabecalhoDeAltenticidade.tsx
@@ -5,7 +5,7 @@ export default function CabecalhoDeAltenticidade(){
 
     return(
         <div className="h-[10vh] w-[100%] flex items-center p-[1rem] border-t border-white/10 hover:bg-white/5 transition-colors rounded-3xl shadow-2xl border border-white/20">
-        <p className="text-[#D9D9D9] font-bold text-[1.3rem]">Bom-vindo(a), {paciente?.nomeCompleto}</p>
+        <p className="text-[#D9D9D9] font-bold text-[1.3rem]">Bem-vindo(a), {paciente?.nomeCompleto}</p>
         
         </div>
     );
